82aa24f20ae028f0b2332bff57d7910ec3112cf5
nouzts <57776126+nouzts@users.noreply.github.com>
2019-11-18 12:46:33 -0800

    Create lesson1.py
    The first lesson for Christine's Lunch and Learn on intro to Programming.

---
+++ lesson1.py
@@ -0,0 +1,11 @@
+# Variable being assigned a string value
+subject = "Christine's Lunch and Learn at Hallmark Labs!!!!!"
+
+# Ask the user for thier name
+user_name = input("What is your name? ")
+
+# Putting together string and a variable value together
+welcome_message =  user_name + ', welcome to ' + subject
+
+# Display message to screen
+print(welcome_message)
